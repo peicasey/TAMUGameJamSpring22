186636e996044869bca7b047162f07bf4183bfd2
PeterSchlenker <91510378+PeterSchlenker@users.noreply.github.com>
2022-03-23 22:17:42 -0500

    New scene, new camera, more orgaization
    I made prefabs from the old scene and opened up a new scene (with not much in it). I re-added the camera following from Time Leap and tweaked the Shoot script so that it works with a perspective camera (that will give us parallax). I also organized our assets into their respective folders.

---
+++ Grappling with School/Assets/Scripts/CharacterAiming.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAiming : MonoBehaviour
+{
+    public event EventHandler<OnShootEventArgs> OnShoot;
+    public class OnShootEventArgs : EventArgs
+    {
+        public Transform firePoint;
+        public Vector3 aimPoint;
+        public bool isArm1;
+    }
+
+    private GameObject myPlayer;
+    [SerializeField] private Transform firePoint;
+    private Vector3 aimPoint;
+    [SerializeField] private bool isArm1;
+    private bool isAiming = true;
+
+    private void Update()
+    {
+        if (isArm1)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                OnShoot?.Invoke(this, new OnShootEventArgs { firePoint = firePoint, aimPoint = aimPoint, isArm1 = isArm1 });
+            }
+        } else
+        {
+            if (Input.GetButtonDown("Fire2"))
+            {
+                OnShoot?.Invoke(this, new OnShootEventArgs { firePoint = firePoint, aimPoint = aimPoint, isArm1 = isArm1 });
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isAiming)
+        {
+            aimPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+            Vector3 difference = aimPoint - transform.position;
+            difference.Normalize();
+            float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+        } else
+        {
+
+        }
+    }
+
+    public bool getIsArm1()
+    {
+        return isArm1;
+    }
+
+    public Transform getFirePoint()
+    {
+        return firePoint;
+    }
+    public Vector3 getAimPoint()
+    {
+        return aimPoint;
+    }
+}
