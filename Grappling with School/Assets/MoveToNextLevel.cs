ec9df8972422e88ad9b84bd014c78444f716acfc
Dallasocoggins <dallasocoggins@gmail.com>
2022-04-23 15:49:50 -0500

    Added End Platform

---
+++ Grappling with School/Assets/MoveToNextLevel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToNextLevel : MonoBehaviour
+{
+    public int nextSceneLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(SceneManager.GetActiveScene().buildIndex == 7)
+            {
+                Debug.Log("YOU WIN THE GAME");
+            }
+            else
+            {
+                //Move to next level
+                SceneManager.LoadScene(nextSceneLoad);
+
+                //Setting int for Index
+                if (nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
+                {
+                    PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+                }
+            }
+        }
+    }
+}
