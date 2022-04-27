2561931c3adcbce09792c17d0330f9f36e5b06a7
peicasey <peicasey@gmail.com>
2022-04-26 23:26:51 -0500

    Added all the menu screens

--- Grappling with School/Assets/Scripts/SceneSwitcher.cs
+++ Grappling with School/Assets/Scripts/SceneSwitcher.cs
@@ -15,13 +15,27 @@ public class SceneSwitcher : MonoBehaviour
         SceneManager.LoadScene("CreditsScreen");
     }
 
-    public void GotoTutorial() {
-        SceneManager.LoadScene("Tutorial"); // might want to rename
+    public void GoToHowToPlay() {
+        SceneManager.LoadScene("HowToScreen"); // might want to rename
     }
+
+
+    public void GoToLevel1() {
+        SceneManager.LoadScene(1); // might want to rename
+    }
+
+    public void GoToLevel2() {
+        SceneManager.LoadScene(2); // might want to rename
+    }
+
+    public void GoToLevel3() {
+        SceneManager.LoadScene(3); // might want to rename
+    }
+
 
     public void GoToLevelSelect() {
         Debug.Log("Go to level select screen -- doesn't exist yet");
-        // SceneManager.LoadScene("LevelSelectScreen");
+        SceneManager.LoadScene("LevelSelectScreen");
     }
 
     public void quitGame() {
